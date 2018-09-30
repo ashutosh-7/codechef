c96c2a21f81579d7b75e799842b54d11317aeee3
Ashutosh Yadav <erashu.yadaw@gmail.com>
2018-09-30 11:12:15 +0530

    Create Chef Restaurant Problem Code: CHEFRES.cpp

---
+++ September Lunchtime 2018/Chef Restaurant Problem Code: CHEFRES.cpp
@@ -0,0 +1,204 @@
+#include <iostream>
+#include<vector>
+#include<bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+void submit()
+{
+int n,m,i,j,k1,k2,p3=1,p4=0,p5=0,p6=0,p7=0,p8=0,p9=0,l1=0,l2=0,l3=0,l4=0,l5=0;
+int low,high,mid1,mid2,key,c5=0,c6=0,p10;
+vector<int>v1;
+vector<int>v2;
+vector<int>v3;
+
+cin>>n;
+cin>>m;
+for(i=0;i<n;i++)
+{
+	cin>>k1;
+	cin>>k2;
+	v1.push_back(k1);
+	v2.push_back(k2);	
+}
+
+for(i=0;i<m;i++)
+{
+	cin>>k1;
+	v3.push_back(k1);
+}
+
+
+sort(v1.begin(),v1.begin()+n);
+sort(v2. begin(),v2.begin()+n);
+/*for(i=0;i<n;i++)
+{
+	cout<<v1[i]<<endl;
+}
+for(i=0;i<m;i++)
+{
+	cout<<v2[i]<<endl;
+}
+*/
+for(p10=0;p10<m;p10++)
+{
+	k1=v3[p10];
+low=0;
+high=n;
+
+c6=0;
+//1st binary search
+while(low<=high)
+   {
+     mid1=(low+high)/2;
+     if(v1[mid1]<k1)
+     {
+         low=mid1+1;
+     }
+     else if(v1[mid1]>k1)
+     {
+         high=mid1-1;
+     }
+     else
+     {
+        c6=1;
+      
+         break;
+         
+     }
+     
+   }
+   
+p7=0;p8=0;
+ 
+ if(c6==1)
+ {
+ 	cout<<"0"<<endl;
+ 	
+
+ 	p7=1;
+ }
+ //2nd binary search
+ 
+ low=0;
+high=n;
+
+while(low<=high)
+   {
+     mid1=(low+high)/2;
+     if(v2[mid1]<k1)
+     {
+         low=mid1+1;
+     }
+     else if(v2[mid1]>k1)
+     {
+         high=mid1-1;
+     }
+     else
+     {
+        c6=1;
+        
+         break;
+         
+     }
+     
+   }
+
+
+
+
+if(c6==1 &&p7==0 )
+{
+	
+ 
+p5=k1;
+//cout<<p5;
+ 
+	for(i=0;i<n;i++)
+ 	{
+ 		p3=v1[i];
+ 		
+ 		if(p5<p3)
+ 		{
+ 			p5=p3-p5;
+ 			break;
+ 		}
+ 	}
+ 	if(p5<=1000)
+ 	{
+ 		cout<<p5<<endl;
+ 		p8=1;
+ 	}
+ 	else{
+ 		cout<<"-1"<<endl;
+ 		p8=1;
+ 	}
+}
+
+if(p7==0 && p8==0)
+{
+	
+	
+	if(k1>v2[n-1])
+	{
+		cout<<"-1"<<endl;
+			p9=1;
+	}
+}
+if(p7==0 && p8==0 && p9==0)
+{
+
+ for(i=0;i<n;i++)
+ {
+ 	
+ 	l1=v1[i];
+ 	l2=v2[i];
+ 	if(k1>l1 && k1<l2)
+ 	{
+ 		cout<<"0"<<endl;
+ 	
+ 		break;
+ 	}
+ 
+ 
+ }
+ 
+ 
+ 
+ 	
+ }
+ if(k1<v1[0])
+ {
+ 	
+ 	l5=v1[0]-k1;
+ 	
+ 	cout<<l5<<endl;
+ }
+ 
+ l5=0;
+ p7=0;p8=0;p9=0;
+
+}
+v1.clear();
+v2.clear();
+v3.clear();
+
+ 
+ }
+ 
+
+
+int main() {
+int t,i1;
+int n,j,num,p=0;
+	
+cin>>t;
+for(i1=0;i1<t;i1++)
+{
+submit();
+}
+//cout<<p<<endl;
+
+
+	return 0;
+}
